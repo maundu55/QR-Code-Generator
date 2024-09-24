bf509fb03ee3ee357543fc51383d65ed777213be
maundu55 <marymaundu.mm@gmail.com>
2024-09-25 01:54:07 +0300

    add error function and keyframe transitions

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ const qrText= document.getElementById('qrText');
 
 
 function generateQR(){
-    qrImage.src =' https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+    if(qrText.value.length > 0){
+        qrImage.src =' https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+        imgBox.classList.add('show-img');
+    }else{
+        qrText.classList.add('error');
+        setTimeout(()=>{
+        qrText.classList.remove('error');  
+        }, 1000)
+    }
+    
 }
