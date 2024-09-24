a33d9ce883e06da0a7855fddb6d026b525d67268
maundu55 <marymaundu.mm@gmail.com>
2024-09-25 01:22:12 +0300

    get QR code value in JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const imgBox = document.getElementById('imgBox');
 const qrImage = document.getElementById('qrImage');
+const qrText= document.getElementById('qrText');
+
+
 
 function generateQR(){
-    
+    qrImage.src =' https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
 }
